e90b3a8af0b64d9ac5a390b1fe1726baa963da77
hasan <nhasan716@gmail>
2019-10-23 19:13:37 +0700

    deploy heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require ('body-parser');
 const Router = require ('./src/Routes/index');
 const server = express ();
-const port = 3000;
+const port = 5000;
 const nodeEnv = 'Development';
 
 server.use(cors());

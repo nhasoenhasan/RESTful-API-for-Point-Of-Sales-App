86b43c67176c1f41861cdb5a1224f20e5575c669
hasan <nhasan716@gmail>
2019-10-23 19:37:22 +0700

    heroku

--- server.js
+++ server.js
@@ -5,14 +5,11 @@ const cors = require('cors');
 const bodyParser = require ('body-parser');
 const Router = require ('./src/Routes/index');
 const server = express ();
-const port = 7000;
+let port = 7000;
 const nodeEnv = 'Development';
 
 server.use(cors());
 
-// app.listen(port|| 3000, function(){
-//   console.log("Express server listening on port %d in %s mode", this.address().port, port);
-// });
 
 server.listen (port || 3000, () => {
   console.log("Express server listening on port %d in %s mode", this.address().port, port);

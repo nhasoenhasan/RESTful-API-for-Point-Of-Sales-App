c1cab6be5c05033bec20aee7c50dcc8d2f14b2ed
nur hasan <nhasan716@gmail.com>
2019-10-29 04:33:41 +0700

    deploy

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -5,16 +5,16 @@ const form = require ('../Helpers/form');
 const verify = require('../middleware/verifyToken');
 const Router = express.Router ();
 
-Router.get('/', (req, res) => {
-  res.json({
-      message: "Welcome to Point Of Sales RESTful API",
-      author: "Nur Hasan",
-      login: "If you already have an account, please login",
-      register: "Register your account today to use Point Of Sales RESTful API"
-  });
-})
+// Router.get('/', (req, res) => {
+//   res.json({
+//       message: "Welcome to Point Of Sales RESTful API",
+//       author: "Nur Hasan",
+//       login: "If you already have an account, please login",
+//       register: "Register your account today to use Point Of Sales RESTful API"
+//   }); 
+// })
 
-Router.use ('/product',verify, product);
+Router.use ('/product', product);
 Router.use('/auth',auth);
 
 module.exports = Router;

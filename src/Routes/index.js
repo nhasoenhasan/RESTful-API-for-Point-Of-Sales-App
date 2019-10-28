237779aede4ed905491ac49c9c41afb59937375f
nur hasan <nhasan716@gmail.com>
2019-10-29 04:25:51 +0700

    Deploy Setting

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -5,16 +5,14 @@ const form = require ('../Helpers/form');
 const verify = require('../middleware/verifyToken');
 const Router = express.Router ();
 
-const validateUser = (req, res, next) => {
-    jwt.verify(req.headers['x-access-token'], secretKey, (err, decoded) => {
-      if (err) {
-        form.failedResponse(res, 400,'Please Resgister/Login First');
-      }else{
-        req.body.id_user = decoded.id;
-        next();
-      }
-    });
-  }
+Router.get('/', (req, res) => {
+  res.json({
+      message: "Welcome to Point Of Sales RESTful API",
+      author: "Nur Hasan",
+      login: "If you already have an account, please login",
+      register: "Register your account today to use Point Of Sales RESTful API"
+  });
+})
 
 Router.use ('/product',verify, product);
 Router.use('/auth',auth);

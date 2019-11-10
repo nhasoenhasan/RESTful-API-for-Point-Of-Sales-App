7b8a2ca287c7257f338ab0c089e790521c7f736b
nur hasan <nhasan716@gmail.com>
2019-11-10 23:06:27 +0700

    query

--- src/Models/product.js
+++ src/Models/product.js
@@ -167,7 +167,7 @@ module.exports = {
 
   //Make Order
   insertOrder: req => {
-    const insertorder="INSERT INTO `order`(`total`) VALUES (?)";
+    const insertorder="INSERT INTO `order`(`total`) VALUES ?";
     const insertdetail="INSERT INTO `detail_order`(`id_order`, `id_product`, `qty`, `sub_total`) VALUES ?";
     const total=req.body.body.subtotal.sub_total;
 
